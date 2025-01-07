9ee793e9f7182f816e76150ef6b38c2ef3c53795
蒋晨光 <jiangchenguang@digital-hangzhou.com>
2025-01-07 22:56:15 +0800

    feat: 首页：添加顶部栏，添加导航按钮

--- src/page/index-page.tsx
+++ src/page/index-page.tsx
@@ -1,11 +1,30 @@
 import { view, route } from 'coco-mvc';
+import Button from '../view/button'
 
 @route('/')
 @view()
 class IndexPage {
   @view()
   h() {
-    return <div>hello coco-mvc</div>;
+    return <div className={'w-full pt-14'}>
+      <div className={'fixed flex flex-row justify-between items-center top-0 left-0 w-full px-8 h-14 border-b border-gray-200'}>
+        v0.0.1-alpha
+      </div>
+      <div className={'flex flex-col items-center py-20'}>
+        <div className={'text-7xl text-amber-800'}>
+          coco-mvc
+        </div>
+        <div className={'text-3xl mt-4'}>
+          基于装饰器的MVC框架，助力构建可扩展的Web应用
+        </div>
+      </div>
+      <div className={'flex justify-center'}>
+        <div className={'mr-10'}>
+          <Button label={'快速开始'}></Button>
+        </div>
+        <Button label={'API文档'}></Button>
+      </div>
+    </div>
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const config = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/.coco/index.tsx',
   module: {
     rules: [
@@ -9,6 +9,24 @@ const config = {
         test: /\.tsx?$/,
         use: [{ loader: 'babel-loader' }],
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', // 将 CSS 注入到 DOM 中
+          'css-loader',   // 解析 CSS 文件
+          {
+            loader: 'postcss-loader', // 使用 PostCSS 插件
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require('tailwindcss'),
+                  require('autoprefixer'),
+                ],
+              },
+            },
+          },
+        ],
       },
     ],
   },
@@ -25,7 +43,10 @@ const config = {
       directory: path.join(__dirname, 'public'),
     },
     compress: true,
-    port: 8000,
+    port: 8001,
+    devMiddleware: {
+      writeToDisk: true,
+    }
   },
 };
 
