470e8c2c7cbdef1ae3de60acd466b33f34edf30a
jcg <jiang.chenguang@outlook.com>
2025-03-11 23:19:24 +0800

    feat: 添加跳转到github

--- src/view/header-bar.tsx
+++ src/view/header-bar.tsx
@@ -3,14 +3,15 @@ import { view } from 'coco-mvc';
 @view()
 class HeaderBar {
 
-  componentWillUnmount() {
-    console.log('HeaderBar componentWillUnmount');
+  handleClick() {
+    window.open('https://github.com/coco-core/coconut-framework');
   }
 
   render() {
     return <div
       className={'fixed flex flex-row justify-between items-center top-0 left-0 w-full px-8 h-14 border-b border-gray-200'}>
       v0.0.1-alpha
+      <div className={'cursor-pointer'} onClick={this.handleClick}>github</div>
     </div>
   }
 }
