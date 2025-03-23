a432d7d0f6246e9c1626b5a567d99f54430d6e1c
jcg <jiang.chenguang@outlook.com>
2025-03-23 23:10:49 +0800

    feat: 通过copy-webpack-plugin复制文件

--- config/config.js
+++ config/config.js
@@ -1,12 +1,20 @@
 const path = require('node:path');
 const process = require("node:process");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const config = {
   "webpack": {
     "output": {
       publicPath: '/',
       "path": path.join(process.cwd(), "docs")
-    }
+    },
+    "plugins": [
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: path.resolve(process.cwd(), "other-assert"), to: path.join(process.cwd(), "docs") },
+        ],
+      }),
+    ]
   }
 }
 
